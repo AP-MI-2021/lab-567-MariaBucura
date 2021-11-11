044673ce62831497f07648a79a3acf730aebd63c
Maria Bucura <ronnie_ssj4@yahoo.com>
2021-11-11 12:56:21 +0200

    functii de test

--- Tests/test1.py
+++ Tests/test1.py
@@ -1,6 +1,7 @@
 import datetime
-from Domain.Cheltuiala2 import getNewPayment
+from Domain.Cheltuiala import getNewPayment
 from Logic.crud import create
+from Logic.operations import maxim_cheltuieli, handle_undo, handle_redo
 
 
 def test_create():
@@ -11,4 +12,52 @@ def test_create():
     create(lst1, 6, 100, datetime.datetime(2021, 12, 4), 'canal')
     assert lst == lst1
 
+def test_maxim_cheltuieli():
+    c1 = getNewPayment(6, 100, datetime.datetime(2021, 12, 4), 'canal')
+    c2 = getNewPayment(23, 150, datetime.datetime(2021, 10, 15), 'canal')
+    lst = []; lst.append(c1); lst.append(c2)
+    assert maxim_cheltuieli(lst, 'canal') == 150
 
+
+def test_handle_undo():
+    lst = [1, 2, 3]
+    undo = []
+    redo = []
+    undo.append(lst[:])
+    lst.pop(1)
+    undo.append(lst[:])
+    lst, undo, redo = handle_undo(lst, undo, redo)
+    assert len(lst) == 3
+    assert len(undo) == 1
+    assert len(redo) == 1
+    lst.append(5)
+    undo.append(lst[:])
+    redo.clear()
+    lst, undo, redo = handle_undo(lst, undo, redo)
+    assert len(lst) == 3
+    assert len(undo) == 1
+    assert len(redo) == 1
+
+
+def test_handle_redo():
+    lst = [1, 2, 3]
+    undo = []
+    redo = []
+    undo.append(lst[:])
+    lst.pop(1)
+    undo.append(lst[:])
+    lst, undo, redo = handle_undo(lst, undo, redo)
+    lst, undo, redo = handle_redo(lst, undo, redo)
+    assert len(lst) == 2
+    assert len(undo) == 2
+    assert len(redo) == 0
+
+
+
+
+
+def run_all_tests():
+    test_create()
+    test_maxim_cheltuieli()
+    test_handle_undo()
+    test_handle_redo()
